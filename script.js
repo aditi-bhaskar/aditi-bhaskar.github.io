8ef4d5d778ca3636a6f915500cfff3ddd00bf109
aditi-bhaskar <aditi.bhaskar@gmail.com>
2022-07-23 00:09:11 -0700

    added keyboard and 8-character solution

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
+
+// let answer = void;
+
+// for (let i = 0; i < 8; i++) {
+//     answer += StringMath.floor(Math.random() * 2);
+// }
+
 let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
+
 console.log(rightGuessString)
 
 function initBoard() {
@@ -14,7 +22,7 @@ function initBoard() {
         let row = document.createElement("div")
         row.className = "letter-row"
         
-        for (let j = 0; j < 5; j++) {
+        for (let j = 0; j < 8; j++) {
             let box = document.createElement("div")
             box.className = "letter-box"
             row.appendChild(box)
